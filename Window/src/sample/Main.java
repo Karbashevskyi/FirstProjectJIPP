3a7bb567cbfe0ceadbd9f7cdde46b35ef97c9e85
Karba <ivankarbashevskyi@gmail.com>
2018-12-16 16:32:08 +0100

    H: Added connector to database and crete in database database for hotel project.

--- Window/src/sample/Main.java
+++ Window/src/sample/Main.java
@@ -7,12 +7,56 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import core.Hotel;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main extends Application {
+
+
+    // JDBC URL, username and password of MySQL server
+    private static final String url = "jdbc:mysql://localhost:3306/hotel";
+    private static final String user = "root";
+    private static final String password = "";
+
+    // JDBC variables for opening and managing connection
+    private static Connection con;
+    private static Statement stmt;
+    private static ResultSet rs;
 
     private Hotel hotel = new Hotel();
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+
+        String query = "select count(*) from room";
+
+        try {
+
+            // opening database connection to MySQL server
+            con = DriverManager.getConnection(url, user, password);
+
+            // getting Statement object to execute query
+            stmt = con.createStatement();
+
+            // executing SELECT query
+            rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                int count = rs.getInt(1);
+                System.out.println("Total number of toom in the table : " + count);
+            }
+
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        } finally {
+            //close connection ,stmt and resultset here
+            try { con.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { stmt.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { rs.close(); } catch(SQLException se) { /*can't do anything */ }
+        }
 
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("sample.fxml"));
@@ -29,6 +73,7 @@ public class Main extends Application {
 
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+
     }
 
 
